25929926d9c740c222e3fede143dfbfdc6ff6934
miyayamaM <pendulum.ngt.4010@gmail.com>
2024-04-23 20:40:05 +0900

    create s3

---
+++ main.tf
@@ -0,0 +1,26 @@
+terraform {
+  // terraformのバージョンを指定
+  required_version = ">= 1.5.1"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  profile = "masaru"
+}
+
+
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-test-bucket"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}

49b5061a6d3a70f0e11558c91fe364df5bc8e1c4
miyayamaM <pendulum.ngt.4010@gmail.com>
2024-04-23 20:51:42 +0900

    feat: set secrets in var

--- main.tf
+++ main.tf
@@ -10,11 +10,14 @@ terraform {
   }
 }
 
-provider "aws" {
-  # Configuration options
-  profile = "masaru"
-}
+variable "aws_access_key" {}
+variable "aws_access_secret_key" {}
 
+provider "aws" {
+  access_key = var.aws_access_key
+  secret_key = var.aws_access_secret_key
+  region = "ap-northeast-1"
+}
 
 resource "aws_s3_bucket" "example" {
   bucket = "my-tf-test-bucket"
